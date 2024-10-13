a25cc92f860f5f50d0157945b079a78ff34d4727
Alex Ponce <alexponce2753@gmail.com>
2024-10-12 19:13:39 -0500

    Utilizando ResponseEntity en los controles

--- src/main/java/com/klaus/core/controllers/ClienteRestController.java
+++ src/main/java/com/klaus/core/controllers/ClienteRestController.java
@@ -1,8 +1,11 @@
 package com.klaus.core.controllers;
 
 import com.klaus.core.domain.Cliente;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -29,6 +32,15 @@ public class ClienteRestController {
                 filter(cliente -> cliente.getUsername().equalsIgnoreCase(userName)).
                 findFirst().orElseThrow();
     }
+    /*********************************ResponseEntity****************************************************/
+
+    @GetMapping("/{userName}")
+    public ResponseEntity<?> getCliente1(@PathVariable String userName){
+        return ResponseEntity.ok(clientes.stream().
+                filter(cliente -> cliente.getUsername().equalsIgnoreCase(userName)).
+                findFirst().orElseThrow());
+    }
+    /***************************************************************************************/
 
     /*
     @GetMapping("/clientes/{userName}")
@@ -46,9 +58,22 @@ public class ClienteRestController {
         clientes.add(cliente);
         return cliente;
     }
+    /****************************ResponseEntity**********************************************/
+    @PostMapping
+    public ResponseEntity<?>agregarCLiente(@RequestBody Cliente cliente){
+        clientes.add(cliente);
+        //obteniendo URL de servicio
+
+        URI location = ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .path("/{userName}")
+                .buildAndExpand(cliente.getNombre())
+                .toUri();
+        return ResponseEntity.created(location).body(cliente);
+    }
+    /*****************************************************************************/
 
     /*Para mdodificar primero se debe de buscar el usuario por el userName -> similar a recuperar userName*/
-
     @PutMapping("/clientes")
     public Cliente madificacionCliente(@RequestBody Cliente cliente){
         Cliente clienteEncontrado = clientes.stream()
@@ -63,4 +88,45 @@ public class ClienteRestController {
         * en caso de que el cliente existe en el sistemas se le setean los nuevos
         * valores para los atributos password y nombre*/
     }
+    /***************************************ResponseEntity**************************************************/
+    @PutMapping
+    public  ResponseEntity<?> modificarCliente1(@RequestBody Cliente cliente){
+        Cliente clienteEncontrado = clientes.stream().
+                filter(cli->cli.getUsername().equalsIgnoreCase(cliente.getUsername())).
+                findFirst().orElseThrow();
+        clienteEncontrado.setPassword(cliente.getPassword());
+        clienteEncontrado.setNombre(cliente.getNombre());
+
+        return ResponseEntity.ok(clienteEncontrado);
+    }
+    /*****************************************************************************************/
+
+    @DeleteMapping("/clientes/{userName}")
+    public void deleteCliente (@PathVariable String userName){
+        Cliente clienteEncontrado = clientes.stream().
+                filter(cli -> cli.getUsername().equalsIgnoreCase(userName)).
+                findFirst().orElseThrow();
+        clientes.remove(clienteEncontrado);
+    }
+
+    @DeleteMapping("{userName}")
+    public void deleteC1iente (@PathVariable String userName){
+        Cliente clienteEncontrado = clientes.stream().
+                filter(cli -> cli.getUsername().equalsIgnoreCase(userName)).
+                findFirst().orElseThrow();
+        clientes.remove(clienteEncontrado);
+    }
+    /*******************************************************************************/
+    @DeleteMapping("/{userName}")
+    public ResponseEntity deleteCliente1(@PathVariable String userName){
+        Cliente clienteEcontrado = clientes.stream().
+                filter(cli-> cli.getUsername().equalsIgnoreCase(userName)).
+                findFirst().orElseThrow();
+        clientes.remove(clienteEcontrado);
+        return ResponseEntity.noContent().build();
+    }
+    /*******************************************************************************/
+
+
+
 }
